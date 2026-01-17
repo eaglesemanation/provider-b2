6a4a6e99442b60b5f5750ac8a312657cc78a177a
Vladimir Romashchenko <eaglesemanation@gmail.com>
2026-01-17 17:47:54 -0500

    Bump dependencies to v2

--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -7,7 +7,7 @@ package controller
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	"github.com/crossplane/upjet/pkg/controller"
+	"github.com/crossplane/upjet/v2/pkg/controller"
 
 	applicationkey "github.com/eaglesemanation/provider-b2/internal/controller/b2/applicationkey"
 	bucket "github.com/eaglesemanation/provider-b2/internal/controller/b2/bucket"
